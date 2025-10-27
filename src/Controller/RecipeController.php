7cc1eacd8892546c20fce70519a1ed8ae9998f9b
alpernuage <alperakbulut@gmail.com>
2025-10-27 10:45:49 +0100

    add Form events

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -46,7 +46,6 @@ final class RecipeController extends AbstractController
         $form = $this->createForm(RecipeType::class, $recipe);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $recipe->setUpdatedAt(new \DateTimeImmutable());
             $em->flush();
             $this->addFlash('success', 'La recette a bien été modifiée');
 
@@ -67,9 +66,6 @@ final class RecipeController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $recipe->setCreatedAt(new \DateTimeImmutable());
-            $recipe->setUpdatedAt(new \DateTimeImmutable());
-
             $em->persist($recipe);
             $em->flush();
             $this->addFlash('success', 'La recette a bien été créée');
