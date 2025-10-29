3070fd5953618128ae32518fdab1bbda24a12a5e
alpernuage <alperakbulut@gmail.com>
2025-10-29 13:56:13 +0100

    centralise form events with FormListenerFactory

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -4,18 +4,19 @@ namespace App\Form;
 
 use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Event\PostSubmitEvent;
-use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class RecipeType extends AbstractType
 {
+    public function __construct(private FormListenerFactory $listenerFactory)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -32,32 +33,8 @@ class RecipeType extends AbstractType
             ->add('save', SubmitType::class, [
                 'label' => 'Envoyer'
             ])
-            ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...))
-            ->addEventListener(FormEvents::POST_SUBMIT, $this->attachTimestamps(...));
-    }
-
-    public function autoSlug(PreSubmitEvent $event): void
-    {
-        $data = $event->getData();
-        if (empty($data['slug'])) {
-            $slugger = new AsciiSlugger();
-            $data['slug'] = strtolower($slugger->slug($data['title']));
-            $event->setData($data);
-        }
-    }
-
-    public function attachTimestamps(PostSubmitEvent $event): void
-    {
-        $data = $event->getData();
-        if (!($data instanceof Recipe)) {
-            return;
-        }
-
-        $data->setUpdatedAt(new \DateTimeImmutable());
-
-        if (!$data->getId()) {
-            $data->setCreatedAt(new \DateTimeImmutable());
-        }
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->listenerFactory->autoSlug('title'))
+            ->addEventListener(FormEvents::POST_SUBMIT, $this->listenerFactory->timestamps());
     }
 
     public function configureOptions(OptionsResolver $resolver): void
